9a147d810e70113a286ea124ad753d79acdc2802
Theycallmeki <laxamana.kian13@gmail.com>
2026-01-24 02:14:50 +0800

    s

--- S-Frontend/src/services/api.js
+++ S-Frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.pimart.software",
+  withCredentials: true,          // 🔥 REQUIRED FOR COOKIE AUTH
   headers: {
     "Content-Type": "application/json",
   },
