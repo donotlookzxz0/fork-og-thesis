c605c4a8f3c4389a7712c919733ad25b510bc4e0
Theycallmeki <laxamana.kian13@gmail.com>
2026-01-24 01:06:46 +0800

    fix

--- S-Frontend/src/services/api.js
+++ S-Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/",        // 🔥 FORCE VERCEL PROXY
+  baseURL: "/api",          // ✅ NO TRAILING SLASH (IMPORTANT)
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
