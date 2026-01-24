bc9b13bed3d59cb379b8d72bbfa7848297a30fd6
Theycallmeki <laxamana.kian13@gmail.com>
2026-01-25 00:19:53 +0800

    debuging axios call

--- C-Frontend/src/api/axios.js
+++ C-Frontend/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("AXIOS BASE URL:", "https://api.pimart.software");
+
 const api = axios.create({
   baseURL: "https://api.pimart.software",
   withCredentials: true,
