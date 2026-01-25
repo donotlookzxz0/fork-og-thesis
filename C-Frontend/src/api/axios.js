8ff7e6114e41f203800041e39ff3aac7478fe816
Theycallmeki <laxamana.kian13@gmail.com>
2026-01-25 09:12:04 +0800

    fixes ng onte

--- C-Frontend/src/api/axios.js
+++ C-Frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("AXIOS BASE URL:", "https://api.pimart.software");
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://api.pimart.software",
   withCredentials: true,
 });
 
