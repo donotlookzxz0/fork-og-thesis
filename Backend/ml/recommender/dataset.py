81ef2fbb607e5dabe3d730656c8b521aa2dd5e8d
donotlook <repodsalgo200zzz@gmail.com>
2026-02-19 14:30:14 +0800

    test: try fix recommender train

--- Backend/ml/recommender/dataset.py
+++ Backend/ml/recommender/dataset.py
@@ -1,10 +1,16 @@
 from collections import defaultdict
 from models.sales_transaction import SalesTransaction
+from sqlalchemy.orm import joinedload
 
 def build_interactions():
     user_item = defaultdict(lambda: defaultdict(int))
 
-    transactions = SalesTransaction.query.all()
+    transactions = (
+        SalesTransaction.query
+        .options(joinedload(SalesTransaction.items))
+        .all()
+    )
+    
     for tx in transactions:
         for ti in tx.items:
             user_item[tx.user_id][ti.item_id] += ti.quantity
